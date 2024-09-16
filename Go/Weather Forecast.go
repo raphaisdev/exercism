a58414ac93e15d3f871500f2063b502e93da2550
Raphael <1247158+raphaisdev@users.noreply.github.com>
2024-09-16 19:48:59 -0300

    Update Weather Forecast.go

--- Go/Weather Forecast.go
+++ Go/Weather Forecast.go
@@ -1,3 +1,14 @@
+/**
+1. Document package weather
+Since goblins are not as smart as you are, they forgot what the package should do for them. Please write a comment for package weather that describes its contents. The package comment should introduce the package and provide information relevant to the package as a whole.
+
+2. Document the CurrentCondition and CurrentLocation variables
+The president of Goblinocus is a bit paranoid and fears uncommented variables are used to destroy their country. Please clarify the usage of the package variables CurrentCondition and CurrentLocation and put the president's mind at ease. This should tell any user of the package what information the variables store, and what they can do with it.
+
+3. Document the Forecast() function
+Goblinocus forecast operators want to know what the Forecast() function does (but do not tell them how it works, since unfortunately, they will get more confused). Please write a comment for this function that describes what the function does, but not how it does it.
+**/
+
 // Package weather provides tools to display information about wheater.
 package weather
 
